e155aefb086226bcb8a90609c2fc5ae52817bd1d
Fish-House-SC <estudiofishhouse@gmail.com>
2024-12-16 17:14:16 -0300

    java

---
+++ aula 11/ex008.js
@@ -0,0 +1,8 @@
+
+var vei = 70.5
+console.log(`A velocidade é ${vei}km/h`)
+if (vei > 60){ //condição simples
+    console.log('Voce ultrapasspou a velocidade permitida MULTADO!')
+}
+
+console.log('Dirija apenas usando sinto de segurança')
